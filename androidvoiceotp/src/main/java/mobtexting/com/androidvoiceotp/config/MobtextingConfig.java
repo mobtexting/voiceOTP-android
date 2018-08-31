64674fa7e6771a7811c849a40fd61f95622f3b5d
Dhananjay <d.gupta@3mdigital.co.in>
2018-08-31 12:33:28 +0530

    base url fixed

--- androidvoiceotp/src/main/java/mobtexting/com/androidvoiceotp/config/MobtextingConfig.java
+++ androidvoiceotp/src/main/java/mobtexting/com/androidvoiceotp/config/MobtextingConfig.java
@@ -1,6 +1,6 @@
 package mobtexting.com.androidvoiceotp.config;
 
 public class MobtextingConfig {
-    public static final String MOBTEXTING_BASE_URL="";
+    public static final String MOBTEXTING_BASE_URL="http://api.mobtexting.in";
     public static final String MOBTEXTING_VOICE_OTP_URL="";
 }
--- app/src/main/java/mobtexting/com/androidvoiceotp/MainActivity.java
+++ app/src/main/java/mobtexting/com/androidvoiceotp/MainActivity.java
@@ -2,12 +2,22 @@ package mobtexting.com.androidvoiceotp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+import mobtexting.com.androidvoiceotp.model.ServerResponse;
+import mobtexting.com.androidvoiceotp.reposotories.MobtextingInterface;
+import mobtexting.com.androidvoiceotp.reposotories.VerificationInterface;
+import mobtexting.com.androidvoiceotp.service.MobtextingService;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,VerificationInterface{
     private EditText phonenumberEt;
     private Button btnOtpVoice;
+    private MobtextingService mobtextingService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +26,34 @@ public class MainActivity extends AppCompatActivity {
         phonenumberEt=(EditText)findViewById(R.id.phonenumberEt);
         btnOtpVoice=(Button)findViewById(R.id.btnOtpVoice);
 
+        mobtextingService=new MobtextingService(getBaseContext());
 
+
+        btnOtpVoice.setOnClickListener(this);
+    }
+
+    public int generateSixDigitRandomNumber(){
+        Random random=new Random();
+        int sixDigit=100000+ random.nextInt(800000);
+        return sixDigit;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (btnOtpVoice.getId()){
+            case R.id.btnOtpVoice:
+                mobtextingService.getVoiceOTP("7250705072",String.valueOf(generateSixDigitRandomNumber()),"7488792140",MainActivity.this);
+            break;
+        }
+    }
+
+    @Override
+    public void onSuccesResponse(ServerResponse serverResponse) {
+        Log.d("sdsadasd",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
+    }
+
+    @Override
+    public void onErrorResponse(ServerResponse serverResponse) {
+        Log.d("dsdsds",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
     }
 }
