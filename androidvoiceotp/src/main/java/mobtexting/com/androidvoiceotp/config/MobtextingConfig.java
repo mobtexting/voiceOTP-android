ec2377470f719d0c46c0806ba5e50ca0c2b97da0
Dhananjay <d.gupta@3mdigital.co.in>
2018-08-31 12:40:04 +0530

    dummy url fixed

--- androidvoiceotp/src/main/java/mobtexting/com/androidvoiceotp/config/MobtextingConfig.java
+++ androidvoiceotp/src/main/java/mobtexting/com/androidvoiceotp/config/MobtextingConfig.java
@@ -2,5 +2,5 @@ package mobtexting.com.androidvoiceotp.config;
 
 public class MobtextingConfig {
     public static final String MOBTEXTING_BASE_URL="http://api.mobtexting.in";
-    public static final String MOBTEXTING_VOICE_OTP_URL="";
+    public static final String MOBTEXTING_VOICE_OTP_URL="/api.v1/json/";
 }
