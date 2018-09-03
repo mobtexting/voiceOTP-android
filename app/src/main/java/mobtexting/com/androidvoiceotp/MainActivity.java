7620840f47abc4f55966f885e6772b5ff76d61d3
Dhananjay <d.gupta@3mdigital.co.in>
2018-09-03 16:17:58 +0530

    added example final

--- app/src/main/java/mobtexting/com/androidvoiceotp/MainActivity.java
+++ app/src/main/java/mobtexting/com/androidvoiceotp/MainActivity.java
@@ -1,8 +1,8 @@
 package mobtexting.com.androidvoiceotp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +12,6 @@ import android.widget.Toast;
 import java.util.Random;
 
 import mobtexting.com.androidvoiceotp.model.ServerResponse;
-import mobtexting.com.androidvoiceotp.reposotories.MobtextingInterface;
 import mobtexting.com.androidvoiceotp.reposotories.VerificationInterface;
 import mobtexting.com.androidvoiceotp.service.MobtextingService;
 
@@ -21,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnOtpVoice;
     private MobtextingService mobtextingService;
     private String sixDigitOTP;
+
+    private String TAG=MainActivity.this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnOtpVoice.setOnClickListener(this);
     }
 
+    /**
+     * generate Six Digit OTP
+     * @return
+     */
     public int generateSixDigitRandomNumber(){
         Random random=new Random();
         int sixDigit=100000+ random.nextInt(800000);
@@ -45,26 +50,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (btnOtpVoice.getId()){
             case R.id.btnOtpVoice:
-                sixDigitOTP=String.valueOf(generateSixDigitRandomNumber());
-                mobtextingService.getVoiceOTP("7250705072",sixDigitOTP,"7488792140",MainActivity.this);
+                if(phonenumberEt.getText().toString().equals("")){
+                    Toast.makeText(getBaseContext(), "Enter Mobile number", Toast.LENGTH_SHORT).show();
+                }else {
+                    sixDigitOTP = String.valueOf(generateSixDigitRandomNumber());
+                    mobtextingService.getVoiceOTP("7250705072", sixDigitOTP, "7488792140", MainActivity.this);
+                }
             break;
         }
     }
 
     @Override
     public void onSuccesResponse(ServerResponse serverResponse) {
-        Log.d("sdsadasd",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
+        Log.d("TAG",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
         if(serverResponse.isStatus()&&serverResponse.getResponse_code()==200){
             Intent verifyActivityIntent=new Intent(getBaseContext(),VerificationActivity.class);
             verifyActivityIntent.putExtra("otp",sixDigitOTP);
             startActivity(verifyActivityIntent);
         }else{
-            Log.d("serverResponse",serverResponse.getMessage()+"   "+serverResponse.getResponse_code());
+            Log.d("TAG",serverResponse.getMessage()+"   "+serverResponse.getResponse_code());
         }
     }
 
     @Override
     public void onErrorResponse(ServerResponse serverResponse) {
-        Log.d("dsdsds",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
+        Log.d("TAG",serverResponse.getMessage()+"  "+serverResponse.getResponse_code()+"  "+serverResponse.isStatus());
     }
 }
